ff92d748c9ce51bbf38234ef4b331b5df761c520
engelja1 <engelja1@ITSD-1WJWDV1-WL.dom1.jhuapl.edu>
2017-06-30 11:34:22 -0400

    Small Changes

--- trajectorymodel.m
+++ trajectorymodel.m
@@ -2,7 +2,7 @@
 clear;
 clc;
 
-timeStep = 0.01; 
+timeStep = 0.01;
 %Time interval
 time = 0:timeStep:(5 - timeStep);
 %Begin timing
@@ -19,22 +19,18 @@ interceptorParams.mass = 0.0427;
 interceptorParams.area = 0.0025652;
 interceptorParams.drag = 0.47;
 interceptorIC = [-45 / sqrt(2),45 / sqrt(2),50,0];
-
 %Differential equations solver calculates the X velocities, Z velocities,
 %X position, and Z position. Stored in an array 'Y'
 [tInterceptor,YInterceptor] = ode45(@(t,Y) trajectory(t,Y,interceptorParams), time, interceptorIC);
 [tThreat,YThreat] = ode45(@(t,Y) trajectory(t,Y,threatParams), time, threatIC);
-
 %Calculate intersections
 [intersectionX, intersectionY] = intersections(YInterceptor(:, 3), YInterceptor(:, 4), YThreat(:, 3), YThreat(:, 4));
 
-%Iterator through the threat path
-%Arbitraly look at x values of threat
+
+%Iterator through the paths
 %Finds the index of the first x greater than the intersection point
-iThreat = 1;
-while YThreat(iThreat,3) < intersectionX
-    iThreat = iThreat + 1;
-end
+%Finds the time at these points
+iThreat = find(YThreat(:, 3) > intersectionX, 1);
 xBefore = YThreat(iThreat - 1, 3);
 xAfter = YThreat(iThreat, 3);
 xDif = xAfter - xBefore;
@@ -42,10 +38,7 @@ xDifToIntersection = intersectionX - xBefore;
 ratio = xDifToIntersection / xDif;
 timeIntersectionThreat = tThreat(iThreat) + ratio * timeStep;
 
-iInterceptor = 1;
-while YInterceptor(iInterceptor,3) > intersectionX
-    iInterceptor = iInterceptor + 1;
-end
+iInterceptor = find(YInterceptor(:, 3) < intersectionX, 1);
 xBefore = YInterceptor(iInterceptor - 1, 3);
 xAfter = YInterceptor(iInterceptor, 3);
 xDif = xAfter - xBefore;
@@ -55,6 +48,8 @@ timeIntersectionInterceptor = tInterceptor(iInterceptor) + ratio * timeStep;
 
 %Stop timing and read time
 timeElapsed = toc;
+toc
+%Find the time until the interceptor needs to be launched
 timeTillLaunch = timeIntersectionThreat - timeIntersectionInterceptor - timeElapsed
 
 %Plot everything
@@ -64,8 +59,8 @@ plot(YThreat(1:iThreat,3), YThreat(1:iThreat,4), 'r');
 plot(YInterceptor(iInterceptor: length(YInterceptor),3), YInterceptor(iInterceptor: length(YInterceptor),4), ':b');
 plot(YInterceptor(iInterceptor: length(YInterceptor),3), YInterceptor(iInterceptor: length(YInterceptor),4), ':b');
 plot(YThreat(iThreat:length(YThreat),3), YThreat(iThreat:length(YThreat),4), ':r');
-
 plot(intersectionX, intersectionY, '-*k');
+
 %Set plot limits
 xlim([-30, 70]);
 ylim([.1, 40]);
