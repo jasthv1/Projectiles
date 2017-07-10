b40f0875c914c286ba58b9c644f2ea3be52bacde
poppedc1 <poppedc1@ITSD-43X8FZ1-WL.dom1.jhuapl.edu>
2017-07-10 10:35:27 -0400

    Added comments

--- kalmanFilter.m
+++ kalmanFilter.m
@@ -1,48 +1,70 @@
+%Initial guesses for position, velocity, and acceleration (meters based)
 startX = 0;
 startVX = 20 * cos(pi / 4);
 startAX = 0;
 startZ = 0;
 startVZ = 20 * cos(pi / 4);
 startAZ = -9.8;
+%Create a matrix to represent all initial values
 thetaLast = [startX; startVX; startAX; startZ; startVZ; startAZ];
+
+%Initial variance matrix
 pLast = zeros(6, 6);%TODO change this
 pLast = 10^2*diag(ones(6,1));
+
+%Process error matrix
 Q = zeros(6, 6); %For now
+%Simulated camera error for measurements
 sigmaX = .5;
 sigmaZ = .5;
 R = zeros(2, 2);
 R(1, 1) = sigmaX^2;
 R(2, 2) = sigmaZ^2;
+
+%Measurement matrix
 H = [1, 0, 0, 0, 0, 0; 0, 0, 0, 1, 0, 0];
 
+%Change in time (represents ideal frames/second and update rate)
 dt = 1/30;
+%Total time
 t = 0:dt:3;
+
+%Empty matrices for filtered values
 x_filtered = zeros(size(t));
 z_filtered = zeros(size(t));
 x_measured = zeros(size(t));
 z_measured = zeros(size(t));
 
+%find the ideal paths of the target and interceptor (only target is used 
 path = getPaths(t, 20);
 
-
+%Filter and update measurements to represent estimated values using Kalman
+%filter
 for i = 1:length(t)
 
-    measurement = getMeasurement(i, sigmaX, sigmaZ, path.threat); %change this
+    %Get the measured values, will be changed to take camera input later
+    measurement = getMeasurement(i, sigmaX, sigmaZ, path.threat); 
+    
+    %Kalman filter
+    %Get the state transformation matrix
     F = getFMatrix(dt);
+    %Predict new state values
     thetaPrediction = F * thetaLast; 
+    %Predict new variance values
     pPrediction = F * pLast * F' + Q;
+    %Predicted measurement
     zPrediction = H * thetaPrediction;
+    %Find measurement residual
     residual = measurement - zPrediction;
+    %Finds kalman adjustments
     S = R + H * pPrediction * H';
     K = pPrediction * H' / S;
-    %thetaLast
+    %Find new predicted values of theta
     thetaLast = thetaPrediction + K * residual;
-    %TODO:  figure out whats going on with theta
-    %pLast = pPrediction - K * S * K';
+    %Find new predicted covariance values
     pLast = (eye(6) - K*H)*pPrediction;
+   
     % Store filtered data:
-    
-    
     x_measured(i) = measurement(1);
     x_filtered(i) = thetaLast(1);
     z_measured(i) = measurement(2);
@@ -50,12 +72,12 @@ for i = 1:length(t)
     
 end
 
-%getFMatrix(dt)
-%K
+%Find the true trajectory of the target
 m_truth = getMeasurement(1:length(t), 0, 0, path.threat);
 x_truth = m_truth(1, :);
 z_truth = m_truth(2, :);
 
+%Plot everything
 figure
 plot(x_measured,z_measured,'bo')
 hold on
@@ -63,6 +85,8 @@ plot(x_truth,z_truth,'-g','linewidth',1)
 plot(x_filtered,z_filtered,'-*r','linewidth',2)
 legend('Measured','Truth','Filtered')
 
+%This method retrieves the state transformation matrix for given time
+%difference
 function F = getFMatrix(dt)
 
 F = [1, dt, .5 * dt^2, 0, 0, 0; 0, 1, dt, 0, 0, 0; 0, 0, 1, 0, 0, 0;
@@ -70,15 +94,11 @@ F = [1, dt, .5 * dt^2, 0, 0, 0; 0, 1, dt, 0, 0, 0; 0, 0, 1, 0, 0, 0;
 
 end
 
+%This function finds measurments with normal error from a given index and function f 
 function m = getMeasurement(i, sigmaX, sigmaZ, f)
-
-% 
-% t = t(:)'; %makes sure t is a row vector
-% 
-% x = 20 * cos(pi / 4) * t + randn(size(t)) * sigmaX;
-% z = 20 * cos(pi / 4) * t - 9.8 * t.^2 / 2 + randn(size(t)) * sigmaZ;
 
 x = f(i,3)' + randn(size(i)) * sigmaX;
 z = f(i,4)' + randn(size(i)) * sigmaZ;
 m = [x; z];
+
 end
