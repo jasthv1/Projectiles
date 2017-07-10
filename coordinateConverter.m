0059672c5eb910334f0f0c76fb2b9867fb85f4e1
goodrot1 <goodrot1@ITSD-26TSKX1-WL.dom1.jhuapl.edu>
2017-07-10 15:26:07 -0400

    fixed signs

--- coordinateConverter.m
+++ coordinateConverter.m
@@ -2,11 +2,11 @@ d = 5; %distance between cameras
 %centroid coordinates
 cenrx = 100;
 cenry = 5;
-cenlx = 60;
-cenly = 8;
+cenlx = -100;
+cenly = 5;
 %camera angles
 alphaz = 10 * pi/180;
-alphax = 45 * pi/180;
+alphax = 10 * pi/180;
 beta = .1276 * pi/180;
 %other angles
 alphal = alphax - cenlx * beta;
@@ -18,8 +18,8 @@ n = sqrt(m^2 + d^2 / 4 - m*d*cos(alphal));
 theta = asin(m*sin(alphal)/n) - pi/2;
 
 %final equations
-y = n*sin(theta);
-x = n*cos(theta);
-zl = x*tan(alphaz + cenly * beta);
-zr = x*tan(alphaz + cenry * beta);
-z = (zl + zr)/2;
+y = n*sin(theta)
+x = - abs( n*cos(theta))
+zl = abs( x*tan(alphaz + cenly * beta));
+zr = abs( x*tan(alphaz + cenry * beta));
+z = (zl + zr)/2
